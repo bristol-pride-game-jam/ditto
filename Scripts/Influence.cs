630fc24a417028e7b67e7ebcc74ed0ce71feff75
Tim Skew <timskew@gmail.com>
2019-07-06 12:58:11 +0100

    Adds component to absorb influence into an exposed V3. Also starts with a weird shader

---
+++ Scripts/Influence.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Influence : MonoBehaviour
+{
+    public float C;
+    public float M;
+    public float Y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 GetInfluenceProportions()
+    {
+        return new Vector3(C, M, Y).normalized;
+    }
+}
---
+++ Scripts/InfluenceAbsorption.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class InfluenceAbsorption : MonoBehaviour
+{
+    public Vector3 Accumulation { get; private set; } = new Vector3();
+
+    public float absorptionRadius = 1.0f;
+    public float maxAbsorptionRate = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AbsorbFromNearbyInfluences();
+    }
+
+    private void AbsorbFromNearbyInfluences()
+    {
+        var influences = FindObjectsOfType<Influence>();
+        var nearbyInfluences = influences.Where(i => Vector3.Distance(transform.position, i.transform.position) <= absorptionRadius);
+        foreach (Influence nearbyInfluence in nearbyInfluences)
+        {
+            Vector3 influenceProportions = nearbyInfluence.GetInfluenceProportions();
+            Accumulation += influenceProportions * maxAbsorptionRate * Time.deltaTime;
+        }
+    }
+}
