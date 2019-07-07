e9e134956ed1563990e90c212bb432439e3e50cd
Tim Skew <timskew@gmail.com>
2019-07-07 15:56:05 +0100

    Things with shrine, level, victory condition

--- Scripts/Shrine.cs
+++ Scripts/Shrine.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Shrine : MonoBehaviour
 {
     public static List<Shrine> shrines = new List<Shrine>();
-    public gameObject player;
+    public GameObject player;
 
     private InfluenceAbsorption playerAura;
     private bool activated = false;
@@ -21,17 +21,18 @@ public class Shrine : MonoBehaviour
             bool won = true;
 
             //Check Victory
-            foreach (Shrine shrine in shrines) {
-                if (!shrine.Activated) {
-                    won = false;
-                }
-            }
+            //foreach (Shrine shrine in shrines) {
+            //    if (!shrine.Activated) {
+            //        won = false;
+            //    }
+            //}
             if (won) Victory();
         }
     }
 
     void Start() {
         shrines.Add(this);
+        player = player ?? FindObjectOfType<InfluenceAbsorption>().gameObject;
         playerAura = player.GetComponent<InfluenceAbsorption>();
     }
 
@@ -45,13 +46,25 @@ public class Shrine : MonoBehaviour
 
     bool CanActivate (InfluenceAbsorption character) {
         return(
-            Mathf.Magnitude(aura.x - character.Accumulation.x) < tolerance &&
-            Mathf.Magnitude(aura.y - character.Accumulation.y) < tolerance &&
-            Mathf.Magnitude(aura.z - character.Accumulation.z) < tolerance
+            Mathf.Abs(aura.x - character.Accumulation.x) < tolerance &&
+            Mathf.Abs(aura.y - character.Accumulation.y) < tolerance &&
+            Mathf.Abs(aura.z - character.Accumulation.z) < tolerance
         );
     }
 
     void Victory () {
+        var particles = player.GetComponent<ParticleSystem>();
+        if (particles != null)
+        {
+            particles.Play();
+        }
 
+        var rigidbody = player.GetComponent<Rigidbody>();
+        rigidbody.useGravity = false;
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.freezeRotation = true;
+
+        player.GetComponent<Renderer>().enabled = false;
+        player.GetComponent<CharController>().enabled = false;
     }
 }
